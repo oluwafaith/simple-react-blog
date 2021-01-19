b8cf4811468fbc495abb2cced9b7ab402e69f370
Okpala Faith <okpalafaith1@gmail.com>
2021-01-19 09:52:21 +0100

    added the nav bar

---
+++ src/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+      <nav className="navbar">
+        <h1>The Dojo Blog</h1>
+        <div className="links">
+          <a href="/">Home</a>
+          <a href="/create" style={{ 
+            color: 'white', 
+            backgroundColor: '#f1356d',
+            borderRadius: '8px' 
+          }}>New Blog</a>
+        </div>
+      </nav>
+    );
+  }
+   
+  export default Navbar;
